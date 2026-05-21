d10ca37b3e06f9282b792057399d4562bd5d96d6
bbgoyalbb <bbgoyal.bb@gmail.com>
2026-05-21 20:30:18 +0530

    perf: restore splitChunks optimization in craco.config.js

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,4 +1,4 @@
-// craco.config.js - Minimal working config
+// craco.config.js - Performance optimization config
 const path = require("path");
 
 module.exports = {
@@ -15,5 +15,55 @@ module.exports = {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
+    plugins: [
+      {
+        plugin: {
+          overrideWebpackConfig: ({ webpackConfig }) => {
+            // Add splitChunks optimization for vendor libraries
+            if (!webpackConfig.optimization) {
+              webpackConfig.optimization = {};
+            }
+            if (!webpackConfig.optimization.splitChunks) {
+              webpackConfig.optimization.splitChunks = {
+                chunks: 'all',
+                cacheGroups: {
+                  vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    priority: 10,
+                    reuseExistingChunk: true,
+                  },
+                  recharts: {
+                    test: /[\\/]node_modules[\\/](recharts|d3|react-fast-compare)[\\/]/,
+                    name: 'vendor-recharts',
+                    priority: 20,
+                    reuseExistingChunk: true,
+                  },
+                  radix: {
+                    test: /[\\/]node_modules[\\/]@radix-ui[\\/]/,
+                    name: 'vendor-radix',
+                    priority: 20,
+                    reuseExistingChunk: true,
+                  },
+                  phosphor: {
+                    test: /[\\/]node_modules[\\/]@phosphor-icons[\\/]/,
+                    name: 'vendor-phosphor',
+                    priority: 20,
+                    reuseExistingChunk: true,
+                  },
+                  dates: {
+                    test: /[\\/]node_modules[\\/](date-fns|react-day-picker)[\\/]/,
+                    name: 'vendor-dates',
+                    priority: 20,
+                    reuseExistingChunk: true,
+                  },
+                },
+              };
+            }
+            return webpackConfig;
+          },
+        },
+      },
+    ],
   },
 };
